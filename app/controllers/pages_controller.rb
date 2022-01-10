f121cda88c14a962c537446611768655e6c62805
Anthony Walsh <awsalesforce2021@gmail.com>
2022-01-10 19:59:52 +0000

    my first rails app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 # controller for pages
 class PagesController < ApplicationController
   def home
+    @today = Date.today.strftime("%B #{Date.today.day.ordinalize} %Y")
   end
 
   def about
   end
 
   def contact
+    @members = ['Ben', 'Alex', 'Kat', 'Celine']
   end
 end
